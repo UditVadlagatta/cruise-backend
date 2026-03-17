a63e0c0b052f9141e2784681643b2aa4c2dfdd60
UditVadlagatta <uditvadlagatta1@gmail.com>
2026-03-17 12:17:44 +0530

    use cloudinary for image upload

--- code/middlewares/upload.js
+++ code/middlewares/upload.js
@@ -1,21 +1,55 @@
-import multer from "multer";
-import path from "path";
+// import multer from "multer";
+// import path from "path";
 
-// storage configuration
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "uploads/");
-  },
-  filename: function (req, file, cb) {
-    const uniqueName = Date.now() + "-" + file.originalname;
-    cb(null, uniqueName);
-  }
+// // storage configuration
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "uploads/");
+//   },
+//   filename: function (req, file, cb) {
+//     const uniqueName = Date.now() + "-" + file.originalname;
+//     cb(null, uniqueName);
+//   }
+// });
+
+// // file filter
+// const fileFilter = (req, file, cb) => {
+//   const allowedTypes = ["image/jpeg", "image/png", "image/jpg"];
+
+//   if (allowedTypes.includes(file.mimetype)) {
+//     cb(null, true);
+//   } else {
+//     cb(new Error("Only images are allowed"), false);
+//   }
+// };
+
+// const upload = multer({
+//   storage,
+//   fileFilter
+// });
+
+// export default upload;
+
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
+import multer from 'multer';
+import { v2 as cloudinary } from 'cloudinary';
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-// file filter
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: 'cruises',
+    allowed_formats: ['jpg', 'jpeg', 'png'],
+  },
+});
+
 const fileFilter = (req, file, cb) => {
   const allowedTypes = ["image/jpeg", "image/png", "image/jpg"];
-
   if (allowedTypes.includes(file.mimetype)) {
     cb(null, true);
   } else {
@@ -23,9 +57,6 @@ const fileFilter = (req, file, cb) => {
   }
 };
 
-const upload = multer({
-  storage,
-  fileFilter
-});
+const upload = multer({ storage, fileFilter });
 
 export default upload;
